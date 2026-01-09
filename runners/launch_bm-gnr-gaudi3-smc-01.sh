e06d117dfc8830ff488dd00b87bc1aaac6992643
William Power <73856408+wpower-work@users.noreply.github.com>
2026-01-09 15:51:59 -0700

    Update launch_bm-gnr-gaudi3-smc-01.sh

--- runners/launch_bm-gnr-gaudi3-smc-01.sh
+++ runners/launch_bm-gnr-gaudi3-smc-01.sh
@@ -18,4 +18,4 @@ docker run --rm --ipc=host --shm-size=16g --network=host --name=$server_name \
 -e HABANA_VISIBLE_DEVICES=all \
 --entrypoint=/bin/bash \
 $IMAGE \
-/gitworkspace/benchmarks/"${EXP_NAME%%_*}_${PRECISION}_gaudi3_docker.sh"
+/workspace/benchmarks/"${EXP_NAME%%_*}_${PRECISION}_gaudi3_docker.sh"
