3e867c96d8f511de91ae3011d0a8457991ff9042
William Power <73856408+wpower-work@users.noreply.github.com>
2026-01-09 15:50:19 -0700

    Update launch_bm-gnr-gaudi3-smc-01.sh

--- runners/launch_bm-gnr-gaudi3-smc-01.sh
+++ runners/launch_bm-gnr-gaudi3-smc-01.sh
@@ -12,7 +12,7 @@ docker run --rm --ipc=host --shm-size=16g --network=host --name=$server_name \
 --privileged --cap-add=sys_nice --runtime=habana \
 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
 -v $HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
--v $GITHUB_WORKSPACE:/gitworkspace/ -w /workspace/vllm/ \
+-v $GITHUB_WORKSPACE:/workspace/ -w /workspace/vllm-fork/ \
 -e HF_TOKEN -e HF_HUB_CACHE -e MODEL -e TP -e CONC -e MAX_MODEL_LEN -e PORT=$PORT \
 -e ISL -e OSL -e PYTHONPYCACHEPREFIX=/tmp/pycache/ -e RANDOM_RANGE_RATIO -e RESULT_FILENAME \
 -e HABANA_VISIBLE_DEVICES=all \
