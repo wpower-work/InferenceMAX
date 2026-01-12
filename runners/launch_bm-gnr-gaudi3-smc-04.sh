ecf3be13d341304ba421d395116d8caf2a2aeef4
William Power <73856408+wpower-work@users.noreply.github.com>
2026-01-12 14:50:15 -0700

    Update launch_bm-gnr-gaudi3-smc-04.sh

--- runners/launch_bm-gnr-gaudi3-smc-04.sh
+++ runners/launch_bm-gnr-gaudi3-smc-04.sh
@@ -1,1 +1,21 @@
+#!/usr/bin/bash
 
+sudo sh -c 'echo 0 > /proc/sys/kernel/numa_balancing'
+
+HF_HUB_CACHE_MOUNT="/shareddata/hf_hub_cache_$(hostname)/"
+PORT=8888
+
+server_name="gaudi-test-server"
+
+set -x
+docker run --rm --ipc=host --shm-size=16g --network=host --name=$server_name \
+--privileged --cap-add=sys_nice --runtime=habana \
+--cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+-v $HF_HUB_CACHE_MOUNT:$HF_HUB_CACHE \
+-v $GITHUB_WORKSPACE:/workspace/ \
+-e HF_TOKEN -e HF_HUB_CACHE -e MODEL -e TP -e CONC -e MAX_MODEL_LEN -e PORT=$PORT \
+-e ISL -e OSL -e PYTHONPYCACHEPREFIX=/tmp/pycache/ -e RANDOM_RANGE_RATIO -e RESULT_FILENAME \
+-e HABANA_VISIBLE_DEVICES=all \
+--entrypoint=/bin/bash \
+$IMAGE \
+/workspace/benchmarks/"${EXP_NAME%%_*}_${PRECISION}_gaudi3_docker.sh"
